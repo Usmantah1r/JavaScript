576c147e684cbefc3765f7f0cda8898626e650b6
Usmantah1r <usmantahir980.1@gmail.com>
2026-01-27 12:16:26 +0500

    the first JS Tutorial file

---
+++ t1.js
@@ -0,0 +1,11 @@
+console.log("This file contains the code for the JavaScript")
+
+let a=5
+let b=null
+let c='stringi'
+let d= Symbol
+let e= true
+let f= BigInt(193)
+let g=undefined
+console.log(a, b,c,d,e,f,g)
+console.log(typeof(a,f))
